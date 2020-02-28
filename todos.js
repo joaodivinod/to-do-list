2ec67fa9950c81cf260e3017cb4b8c9ffb7f43c9
João Divino <joaodivinod@gmail.com>
2020-02-28 16:38:04 -0300

    Implementada a função de deletar itens da lista

--- todos.js
+++ todos.js
@@ -1,23 +1,48 @@
 let listElement = document.querySelector('#app ul');
-let importElement = document.querySelector('#app input');
+let inputElement = document.querySelector('#app input');
 let buttonElement = document.querySelector('#app button');
-
 var todos = [
-    'fazer café',
-    'ligar o PC',
-    'abrir o Vs code',
-    'Estudar Js'
+   
 ];
 
 function renderTodos(){
+    listElement.innerHTML = ' ';
     for(todo of todos){
         console.log(todo)
         let todoElement = document.createElement('li');
         let todoText = document.createTextNode(todo);
 
+        let linkElement = document.createElement('a');
+        linkElement.setAttribute('href','#')
+
+        let pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo('+pos+')')
+
+        let linkText = document.createTextNode('excluir');
+
+        linkElement.appendChild(linkText);
+
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
         listElement.appendChild(todoElement)
     }
 }
 
 renderTodos();
+
+function addTodo(){
+    let todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+
+    renderTodos();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos){
+    todos.splice(pos,1)
+    renderTodos();
+}
