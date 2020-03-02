6367c9ee150fe2d224c6d305dfa8b0be4363e372
João Divino <joaodivinod@gmail.com>
2020-03-02 13:31:48 -0300

    Estilo finalizado

--- todos.js
+++ todos.js
@@ -8,23 +8,23 @@ function renderTodos() {
     listElement.innerHTML = '';
 
     for (todo of todos) {
+        var linkElement = document.createElement('a');
+
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
-        var linkElement = document.createElement('a');
 
         linkElement.setAttribute('href', "#");
 
         var pos = todos.indexOf(todo);
         linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
 
-        var linkText = document.createTextNode('Excluir');
+        var linkText = document.createTextNode('✔️');
 
         linkElement.appendChild(linkText);
 
-
-        todoElement.appendChild(todoText);
         todoElement.appendChild(linkElement);
+        todoElement.appendChild(todoText);
         listElement.appendChild(todoElement);
     }
 }
